45d9cfb9b1cd4e70053b96dc13289a7ddf08e347
muskanshrma <muskan.sharma@infostride.com>
2022-09-16 04:24:27 -0700

    TCTC001

--- src/test/java/Pages/PageFactory.java
+++ src/test/java/Pages/PageFactory.java
@@ -4,8 +4,15 @@ import org.openqa.selenium.WebDriver;
 
 public class PageFactory {
     WebDriver driver;
+    private AddToCart addToCart;
 
     public PageFactory(WebDriver driver) {
         this.driver = driver;
+    }
+    public AddToCart getAddToCart() {
+        if (addToCart == null) {
+            addToCart = new AddToCart(driver);
+        }
+        return addToCart;
     }
 }
